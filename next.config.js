f5e15d48c574c3c78835269462400f00bb6392ad
Kevin Nicolas <kncalderong@gmail.com>
2023-08-25 10:58:29 -0500

    feat: next config modified to handle images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'localhost',
+        port: '1337',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+    domains: ['localhost', '127.0.0.1'],
+  },
+}
 
 module.exports = nextConfig
